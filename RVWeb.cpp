c8db212c4f5e0a669d023e81921439f99bff0689
vikaparra <vikaparra@gmail.com>
2022-05-15 17:12:00 -0300

    Tentando utilizar Parser

--- RVWeb.cpp
+++ RVWeb.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 int main() {
     pugi::xml_document doc;
-
-    pugi::xml_parse_result result = doc.load_file("PX_PH_config.xml");
+    pugi::xml_parse_result result = doc.load_file("C:\\Users\\k\\Documents\\Unicamp\\IC\\ReservoirViewerWeb\\RVWeb\\xmlParser\\PX_PH_config.xml");
 
     std::cout << "Load result: " << result.description() << ", mesh name: " << doc.child("mesh").attribute("name").value() << std::endl;
 	cout << "hello!" << endl; // prints
