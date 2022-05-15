3c453466f5a84559c120bc6be8656c0a2dc8e052
vikaparra <vikaparra@gmail.com>
2022-05-15 16:44:01 -0300

    Testando Parser

--- RVWeb.cpp
+++ RVWeb.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main() {
+    pugi::xml_document doc;
+
+    pugi::xml_parse_result result = doc.load_file("PX_PH_config.xml");
+
+    std::cout << "Load result: " << result.description() << ", mesh name: " << doc.child("mesh").attribute("name").value() << std::endl;
 	cout << "hello!" << endl; // prints
 	return 0;
 }
