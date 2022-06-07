c21a73726edeb778bccdaadf65ad80b7f3f1024b
vikaparra <vikaparra@gmail.com>
2022-06-07 13:19:55 -0300

    Add Plugin files

---
+++ src/Plugin/webviz_plugin_boilerplate/plugins/_some_custom_plugin.py
@@ -0,0 +1,84 @@
+from dash import html
+import subprocess as sub
+from webviz_config import WebvizPluginABC
+import os
+
+
+class SomeCustomPlugin(WebvizPluginABC):
+
+    def __init__(self,
+        root: str,
+        benchmark: str,
+        folder2d: str,
+        folderDistMatr: str,
+        chartType: str,
+        layoutCurve: str,
+        clusteringMethod: str,
+        distanceMatrix: str,
+        minClusters: str,
+        maxClusters: str,
+        iterations: str,
+        properties: list,
+        strategies: str,
+        allModels: str,
+    highlightedModels: str) -> None:
+
+        super().__init__()
+
+        self.root = root
+        self.benchmark = benchmark
+        self.folder2d = folder2d
+        self.folderDistMatr = folderDistMatr
+        self.chartType = chartType
+        self.layoutCurve = layoutCurve
+        self.clusteringMethod = clusteringMethod
+        self.distanceMatrix = distanceMatrix
+        self.minClusters = minClusters
+        self.maxClusters = maxClusters
+        self.iterations = iterations
+        self.properties = properties
+        self.strategies = strategies
+        self.allModels = allModels
+        self.highlightedModels = highlightedModels
+
+        full_path = os.path.realpath(__file__)
+        path = os.path.dirname(full_path) + "//foo.exe"
+
+        args = [path,
+        self.root,
+        self.benchmark,
+        self.folder2d,
+        self.folderDistMatr,
+        self.chartType,
+        self.layoutCurve,
+        self.clusteringMethod,
+        self.distanceMatrix,
+        self.minClusters,
+        self.maxClusters,
+        self.iterations,
+        str(self.properties),
+        self.strategies,
+        self.allModels,
+        self.highlightedModels]
+
+        sub.run(args)
+
+
+    @property
+    def layout(self):
+        return html.Div(
+            [self.root,
+        self.benchmark,
+        self.folder2d,
+        self.folderDistMatr,
+        self.chartType,
+        self.layoutCurve,
+        self.clusteringMethod,
+        self.distanceMatrix,
+        self.minClusters,
+        self.maxClusters,
+        self.iterations,
+        self.strategies,
+        self.allModels,
+        self.highlightedModels]
+        )
