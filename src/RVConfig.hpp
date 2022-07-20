5717ed653c6a1feb1baf3e47c7cb30416044dcb1
izzy-el <jv.izael@hotmail.com>
2022-07-19 22:12:49 -0300

    fix: Added #include guard on RVConfig header file

--- src/RVConfig.hpp
+++ src/RVConfig.hpp
@@ -1,24 +1,36 @@
-#include <string>
 #include <iostream>
+#include <string>
+
+#ifndef RVWEB_RVCONFIG_H
+#define RVWEB_RVCONFIG_H
 
 class Configuration {
-    private:
-        std::string root;
-        std::string benchmark;
-        std::string folder2d;
-        std::string folderDistMatr;
-        std::string chartType;
-        std::string layoutCurve;
-        std::string clustMethod;
-        std::string distMatrix;
-        std::string minClusters;
-        std::string maxClusters;
-        std::string numIterations;
-        std::string properties;
-        std::string strategies;
-        std::string allModels;
-        std::string hlModels;
-    public:
-        Configuration::Configuration(std::string root, std::string benchmark, std::string folder2d, std::string folderDistMatr, std::string chartType, std::string layoutCurve, std::string clustMethod, std::string distMatrix, std::string minClusters, std::string maxClusters, std::string numIterations, std::string properties, std::string strategies, std::string allModels, std::string hlModels);
-        std::string getRoot() { return (this->root); }
+private:
+  std::string root;
+  std::string benchmark;
+  std::string folder2d;
+  std::string folderDistMatr;
+  std::string chartType;
+  std::string layoutCurve;
+  std::string clustMethod;
+  std::string distMatrix;
+  std::string minClusters;
+  std::string maxClusters;
+  std::string numIterations;
+  std::string properties;
+  std::string strategies;
+  std::string allModels;
+  std::string hlModels;
+
+public:
+  Configuration(std::string root, std::string benchmark, std::string folder2d,
+                std::string folderDistMatr, std::string chartType,
+                std::string layoutCurve, std::string clustMethod,
+                std::string distMatrix, std::string minClusters,
+                std::string maxClusters, std::string numIterations,
+                std::string properties, std::string strategies,
+                std::string allModels, std::string hlModels);
+  std::string getRoot() { return (this->root); }
 };
+
+#endif
