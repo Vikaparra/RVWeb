2be9e34a615cec256b631afd520d5fd5f2be2024
vikaparra <vikaparra@gmail.com>
2022-07-24 01:13:42 -0300

    Fix: Boost funcionando

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,12 @@
-#include "CSVParser.hpp"
-#include "Metadata.hpp"
-#include <functional>
+// Your First C++ Program a
+#include "RVConfig.hpp"
 #include <iostream>
-#include <boost/archive/binary_oarchive.hpp>
-#include <boost/archive/binary_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
 
-Metadata *Metadata::instance = 0;
-
-int main(int argc, char *argv[])
-{
-  CSVParser csvParser;
-  Metadata *bf = bf->getInstance();
-  std::string filePath = argv[1];
-  std::string column = argv[2];
-
-  std::vector<double> vector;
-
-  bf->setEnsembleName("Triage");
-
-  csvParser.readSpecificColumn(filePath, "I", &vector);
-  bf->setIColumn(vector);
-  bf->setMaxI(vector);
-
-  csvParser.readSpecificColumn(filePath, "J", &vector);
-  bf->setJColumn(vector);
-  bf->setMaxJ(vector);
-
-  csvParser.readSpecificColumn(filePath, "Model", &vector);
-  bf->setModelColumn(vector);
-
-  // csvParser.printVector(vector);
-  std::cout << "This is the last I element: " << bf->getMaxI() << std::endl;
-  std::cout << "This is the last J element: " << bf->getMaxJ() << std::endl;
-
-  std::ofstream ofs("intermediary_file.dat", std::ios::binary);
-
-  {
-    boost::archive::binary_oarchive oa(ofs);
-    oa << bf;
-  }
-
-  return 0;
-};
+int main(int argc, char** argv) {
+    Configuration configuration(argv);
+    // configuration.getProperties();
+    // configuration.getStrategies();    
+    std::cout << "Hello World";
+    return 0;
+}
